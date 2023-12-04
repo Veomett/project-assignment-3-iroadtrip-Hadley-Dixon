967b1fb66d4a1f6db5a15615190e226d2af19f31
Hadley Dixon <hadley.dixon22@gmail.com>
2023-12-03 21:51:02 -0800

    txt file read in done

--- IRoadTrip.java
+++ IRoadTrip.java
@@ -5,8 +5,8 @@ import java.io.*;
 
 public class IRoadTrip {
 
-    private Map<String, Map<String, Integer>> adjacencyList; // Graph of adjacent countries
-    private Map<String, String> stateNameMap; // Map of country names
+    private Map<String, Map<String, Integer>> countryGraph; // Graph of adjacent countries
+    private Map<String, String> countryNameMap; // Map of country names
 
 
 
@@ -19,8 +19,8 @@ public class IRoadTrip {
         }
 
         // Valid input
-        adjacencyList = new HashMap<>(); // Initialize adjacent country graph
-        stateNameMap = new HashMap<>(); // Initialize map of country names
+        countryGraph = new HashMap<>(); // Initialize adjacent country graph
+        countryNameMap = new HashMap<>(); // Initialize map of country names
 
         // Read in files
         txtRead(args[0]); // borders.txt
@@ -28,18 +28,39 @@ public class IRoadTrip {
         tsvRead(args[2]); // state_name.tsv
     }
 
-    // TODO: Read in .txt file
-    private void txtRead(String filename) {
+    private void txtRead(String fileName) {
+        try (Scanner scan = new Scanner(new File(fileName))) { // Scanner class: https://www.geeksforgeeks.org/different-ways-reading-text-file-java/
+            while (scan.hasNextLine()) { // As long as a next line exists...
+                String fileLine = scan.nextLine(); // read line by line
+                String[] lineParts = fileLine.split("="); // split when '=' character
+                String countryName = lineParts[0].trim(); // before '=' is country name
 
+                if (!countryGraph.containsKey(countryName)) { // If the country is not already in our graph,
+                    countryGraph.put(countryName, new HashMap<>()); // Add country to the graph
+                }
+
+                if (lineParts.length > 1) { // If the country has neighbors
+                    String[] neighborArr = lineParts[1].split(";"); // Create an array of neighboring countries
+
+                    for (String neighboringCountry : neighborArr) {
+                        String[] neighborStats = neighboringCountry.trim().split(" "); // Split neighbor name from border distance
+                        countryGraph.get(countryName).put(neighborStats[0], 0); // Default border distance 0
+                    }
+                }
+            }
+        } catch (Exception exception) { // Exceptions: https://www.tutorialspoint.com/javaexamples/exception_method.htm
+            System.err.println(exception.getMessage());
+            System.exit(1);
+        }
     }
 
     // TODO: Read in .csv file
-    private void csvRead(String filename) {
+    private void csvRead(String fileName) {
 
     }
 
     // TODO: Read in .tsv file
-    private void tsvRead(String filename) {
+    private void tsvRead(String fileName) {
 
     }
 
