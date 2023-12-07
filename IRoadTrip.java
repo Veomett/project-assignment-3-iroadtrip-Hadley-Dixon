2e3e477d4c74ca79ba06678e7f4cc1609ea16ccf
Hadley Dixon <hadley.dixon22@gmail.com>
2023-12-06 21:16:18 -0800

    comments and readme

--- IRoadTrip.java
+++ IRoadTrip.java
@@ -12,7 +12,11 @@ public class IRoadTrip {
     private Map<String, String> countryNameMap; // Map of country names
 
 
-    // TODO: Javadoc
+    /**
+     * Constructor for the IRoadTrip class. Method reads the files and prepares to execute the implementation to
+     * respond to requests. The implementation must halt on any failure here.
+     * @param args String array that contains the names of the files as provides to the main.
+     */
     public IRoadTrip (String [] args) {
         // Handle when invalid input
         if (args.length != 3) {
@@ -30,8 +34,11 @@ public class IRoadTrip {
         csvRead(args[1]); // capdist.csv
     }
 
-    // TODO: Javadoc
-    // Helper function to navigate different references to the same country
+    /**
+     * Method accounts for different references to the same country and transforms country name to its 'official' title.
+     * @param inputCountry String initial country name
+     * @return String 'official' country name. See README.md for the 'official' name requirements
+     */
     private String transform(String inputCountry) {
         if (Objects.equals(inputCountry, "Surinam")) {
             return "Suriname";
@@ -92,29 +99,34 @@ public class IRoadTrip {
         } else if (Objects.equals(inputCountry, "UK")) {
             return "United Kingdom";
         }
-        return inputCountry;
+        return inputCountry; // Returns official country name
     }
 
-    // TODO: Javadoc
-    // Creates a map of country names and their unique 3-letter IDs corresponding to 2020, but only if the country is present in both state_name,csv and borders.txt (that way we can identify the 3-letter unique ID for capdist application, and not waste time
+    /**
+     * Method reads 'state_name.tsv' and creates a map of country names and their unique 3-letter IDs.
+     * See README.md for details on country specifications.
+     * @param file String file name
+     */
     private void tsvRead(String file) {
-        try (Scanner scan = new Scanner(new File(file))) { // See source (5) in README.md
-            if (scan.hasNextLine()) { // Read in next line if exists
+        try (Scanner scan = new Scanner(new File(file))) {
+            // Read in next line if exists
+            if (scan.hasNextLine()) {
                 scan.nextLine();
             }
 
-            while (scan.hasNextLine()) { // As long as a next line exists...
+            while (scan.hasNextLine()) { // Next line exists
                 String fileLine = scan.nextLine();
                 String[] lineParts = fileLine.split("\t"); // REGEX: Split at tab
-                if (lineParts.length == 5 && Objects.equals(lineParts[4], "2020-12-31")) {
+                if (lineParts.length == 5 && Objects.equals(lineParts[4], "2020-12-31")) { // Extract only 2020 data
                     String encodedName = lineParts[1].trim(); // The encoded name for a country (eg. JAM)
                     String decodedName = lineParts[2].trim(); // The decoded name for a country (eg. Jamaica)
-                    String transformedDecodedName = transform(decodedName);
-                    countryNameMap.put(transformedDecodedName, encodedName);
+                    String transformedDecodedName = transform(decodedName); // Transform country name to official title
+                    countryNameMap.put(transformedDecodedName, encodedName); // Assign key:value in country name map
 
                 }
             }
 
+            // TESTING
             System.out.println("If 3-letter country ID from 2020 is present in 'state_name.tsv', country is added to map of valid country names");
             System.out.println(countryNameMap);
             System.out.println();
@@ -125,8 +137,11 @@ public class IRoadTrip {
         }
     }
 
-    // TODO: Javadoc
-    // Creates a graph of the countries in borders.txt (which we have 2020 3-letter ID from in state_name) and their associated adjacent countries
+    /**
+     * Method reads 'borders.txt' and creates a graph of countries and their adjacent nations.
+     * See README.md for details on country specifications.
+     * @param file String filename
+     */
     private void txtRead(String file) {
         try (Scanner scan = new Scanner(new File(file))) { // See source (2) in README.md
             while (scan.hasNextLine()) { // As long as a next line exists...
@@ -160,8 +175,11 @@ public class IRoadTrip {
         }
     }
 
-    // TODO: Javadoc
-    // Helper method to get a country's unique 3-letter ID
+    /**
+     * Method gets a country's unique 3-letter ID from the country name map.
+     * @param ID String 3-letter country ID
+     * @return Official name of country corresponding to input ID
+     */
     private String getCountry(String ID) {
         for (String country : countryNameMap.keySet()) {
             if (Objects.equals(countryNameMap.get(country), ID)) {
@@ -170,7 +188,15 @@ public class IRoadTrip {
         } return null;
     }
 
-    // TODO: Javadoc
+    /**
+     * Method reads 'capdist.csv' and extracts distances between capitals of 2 adjacent countries, and adds it to
+     * existing adjacent country graph.
+     * Map is generated with countries that meet the following requirements are added to map, sequentially:
+     * (1) Country 3-letter ID is specified in 'state_name.tsv'
+     * (2) Country also exists in 'border.txt'
+     * (3) Country found in both previous files, has 2020 distance data in 'capdist.csv'
+     * @param file String file name
+     */
     private void csvRead(String file) {
         try (Scanner scan = new Scanner(new File(file))) { // See source (4) in README.md
             if (scan.hasNextLine()) { // Read in next line if exists
@@ -183,6 +209,7 @@ public class IRoadTrip {
                 String idCountryB = lineParts[3].trim(); // Unique ID for country B
                 int capitalDistance = Integer.parseInt(lineParts[4].trim()); // Distance between capitals of country A and country B in km
 
+                // Hardcode to handle edge cases
                 if (idCountryA.equals("UK")) {
                     idCountryA = "UKG";
                 } else if (idCountryB.equals("UK")) {
@@ -210,19 +237,36 @@ public class IRoadTrip {
         }
     }
 
-    // TODO: Javadoc
+    /**
+     * Method returns -1 if the countries don't share a border, and return the distance between the two countries'
+     * capitals if they do share a border.
+     * @param country1 String name of a country
+     * @param country2 String name of a country
+     * @return int distance between the capitals of country1 and country2, -1 otherwise
+     */
     public int getDistance(String country1, String country2) {
         // TODO: getDistance
         return -1;
     }
 
-    // TODO: Javadoc
+    /**
+     * Method returns the path that Dijkstra's algorithm finds, which is the shortest path between two countries,
+     * starting in a capital, ending in a capital, and going through the capital of any intermittent countries. Method
+     * reports distance along the path.
+     * @param country1 String name of a country
+     * @param country2 String name of a country
+     * @return The shortest from country1 to country2 through respective capitals along the path.
+     */
     public List<String> findPath(String country1, String country2) {
         // TODO: findPath
         return null;
     }
 
-    // TODO: Javadoc
+    /**
+     * Method allows a user to interact with your implementation on the console. Receives and validates the names of
+     * two countries from a user. Method does not accept invalid names. Once two valid country names have been entered
+     * by the user, method prints the path between those countries if such a path exists.
+     */
     public void acceptUserInput() {
         Scanner scan = new Scanner(System.in); // See source (6) in README.md
 
@@ -289,72 +333,16 @@ public class IRoadTrip {
         } scan.close(); // Close scanner after the loop
     }
 
-    /*public void acceptUserInput() {
-        Scanner scan = new Scanner(System.in); // See source (6) in README.md
-
-        boolean exit = false;
-
-        while (!exit) {
-            System.out.print("Enter the name of the first country (type EXIT to quit): ");
-            String country1 = scan.nextLine().trim();
-
-            if (country1.equalsIgnoreCase("EXIT")) {
-                exit = true;
-            } else {
-
-                if (!countryGraph.containsKey(country1)) {
-                    System.out.println("Invalid country name. Please enter a valid country name.");
-                    continue;
-                }
-
-                if (country1.equals("Kosovo") || country1.equals("South Sudan") ) {
-                    System.out.println("Valid country in graph, however no available data in distance file.");
-                    System.out.println("Please enter a different country name.");
-                    continue;
-                }
-
-                System.out.print("Enter the name of the second country (type EXIT to quit): ");
-                String country2 = scan.nextLine().trim();
-
-                if (country2.equalsIgnoreCase("EXIT")) {
-                    exit = true;
-                } else {
-
-                    if (!countryGraph.containsKey(country2)) {
-                        System.out.println("Invalid country name. Please enter a valid country name.");
-                        continue;
-                    }
-
-                    if (country2.equals("Kosovo") || country2.equals("South Sudan") ) {
-                        System.out.println("Valid country in graph, however no available data in distance file.");
-                        System.out.println("Please enter a different country name.");
-                        continue;
-                    }
-
-                    // Find path between 2 valid countries
-                    List<String> travelPath = findPath(country1, country2);
-
-                    if (!travelPath.isEmpty()) { // Valid travel path between countries
-                        System.out.println("Route from " + country1 + " to " + country2 + ":");
-                        for (String country : travelPath) { // Step through countries along path
-                            System.out.println("* " + country);
-                        }
-                    } else { // No valid travel path between countries
-                        System.out.println("No path found between " + country1 + " and " + country2);
-                    }
-                }
-            }
-            scan.close();
-        }
-    }*/
-
-    // TODO: Javadoc
-    // Main code provided
+    /**
+     * Main code provided; IRoadTrip class will be called through the main function as follows:
+     * 'java IRoadTrip borders.txt capdist.csv state_name.tsv'
+     * @param args String array of file names.
+     */
     public static void main (String[] args){
         IRoadTrip a3 = new IRoadTrip(args);
-        a3.getDistance("USF", "My House");
-        a3.getDistance("France", "Spain");
-        a3.getDistance("Canada", "Panama");
+        a3.getDistance("USF", "My House"); // TEST
+        a3.getDistance("France", "Spain"); // TEST
+        a3.getDistance("Canada", "Panama"); // TEST
         a3.acceptUserInput();
     }
 }
